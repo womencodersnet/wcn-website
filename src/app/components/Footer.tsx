69101b502ac0cbaab8cb4e61897e7f77a3d73e21
Amelia Dutta <amelia280111dutta@gmail.com>
2025-04-04 00:40:28 +0530

    [FEATURE]-4 converted the footer component hard coded styles to tailwind

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,40 +6,22 @@ const year = new Date().getFullYear();
 
 const Footer: React.FC = () => {
   return (
-    <footer style={{
-        width: '100%',
-        backgroundColor: '#402B6D', 
-        color:'#C0F862',
-        padding: '3rem ',
-        fontFamily: '"Roboto Mono", monospace'
-        }}
-    >
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-            <button style={{
-                height:'min-content',
-                backgroundColor:'#C0F862',
-                color:'#402B6D',
-                padding:'0.5rem',
-                borderRadius:'5px',
-                border:'1px solid #000',
-                boxShadow:'0 0 5px #000',
-                fontSize:'20px',
-                fontWeight:'bold'
-                }}
-            >
+    <footer className='w-full p-10 bg-dark-violet text-leafy font-main'>
+        <div className='flex justify-between'>
+            <button className='h-min bg-leafy text-dark-violet p-2 rounded border-2 border-solid border-[#000]-800 shadow-xl text-xl font-bold'>
                 Our Code of Conduct
             </button>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'start',gap:'1rem'}}>
-                <h3 style={{fontSize:'1.3rem',fontWeight:'bold'}}>Contact Us</h3>
-                <a href="mailto:womencodersnetwork@gmail.com" style={{display:'flex',gap:'0.5rem',alignItems:'center'}}>
+            <div className='flex flex-col items-start gap-3'>
+                <h3 className='text-2xl font-bold'>Contact Us</h3>
+                <a className='flex gap-2 items-center' href="mailto:womencodersnetwork@gmail.com">
                     <IoMdMail />  womencodersnetwork@gmail.com
                 </a>
-                <a href="https://www.linkedin.com/company/womencodersnet/" target='_blank' rel='noreferrer' style={{display:'flex',gap:'0.5rem',alignItems:'center'}}>
+                <a className='flex gap-2 items-center' href="https://www.linkedin.com/company/womencodersnet/" target='_blank' rel='noreferrer'>
                     <FaLinkedin /> WCN Linkedin
                 </a>
             </div>
         </div> 
-      <p style={{position:'relative',bottom:'-2em',textAlign: 'center'}}>&copy; All rights reserved. Women Coders Network {year}</p>
+      <p className='relative bottom-[-2em] text-center'>&copy; All rights reserved. Women Coders Network {year}</p>
     </footer>
   );
 };
