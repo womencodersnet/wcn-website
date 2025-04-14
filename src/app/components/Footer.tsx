ccc408f00ff8cabde101125c8251d97a2fbac70c
Amelia Dutta <amelia280111dutta@gmail.com>
2025-04-14 21:44:07 +0530

    [Feature-53] create mobile layout for footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,11 +7,11 @@ const year = new Date().getFullYear();
 const Footer: React.FC = () => {
   return (
     <footer className='w-full p-10 bg-dark-violet text-leafy font-main'>
-        <div className='flex justify-between'>
-            <button className='h-min bg-leafy text-dark-violet p-2 rounded border-2 border-solid border-[#000]-800 shadow-xl text-xl font-bold'>
+        <div className='flex justify-between max-[640px]:flex-col'>
+            <button className='h-min bg-leafy text-dark-violet p-2 rounded border-2 border-solid border-[#000]-800 shadow-xl text-xl font-bold max-[640px]:mb-4'>
                 Our Code of Conduct
             </button>
-            <div className='flex flex-col items-start gap-3'>
+            <div className='flex flex-col items-start gap-3 max-[640px]:items-center'>
                 <h3 className='text-2xl font-bold'>Contact Us</h3>
                 <a className='flex gap-2 items-center' href="mailto:womencodersnetwork@gmail.com">
                     <IoMdMail />  womencodersnetwork@gmail.com
