88efc0a407d6bb830d4c2b708589c1d43c115bc9
Amelia Dutta <amelia280111dutta@gmail.com>
2025-05-05 21:22:25 +0530

    TASK-55 Created a separate file to organze website infos

---
+++ src/app/WCNInfo.tsx
@@ -0,0 +1,18 @@
+
+export interface WCNInfoProps {
+    email: string;
+    linkedIn: string;
+    year: number;
+}
+
+export const WCNInfo: WCNInfoProps = {
+    email: "womencodersnetwork@gmail.com",
+    linkedIn: "https://www.linkedin.com/company/womencodersnet/",
+    year: new Date().getFullYear(),
+};
+
+
+
+
+
+
