b7a460028f3cec8dc736542a7bd265753f9bb6a7
Amelia Dutta <amelia280111dutta@gmail.com>
2025-05-22 19:48:19 +0530

    [Feature-22] Created the potential About Section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <section className='flex flex-col items-center gap-4 mb-4'>
-        <h3 className='text-3xl font-bold'>About Us</h3>
-        <article className='font-main text-xl'>The Women Coders Network is a community that fosters connection, ignites knowledge sharing, celebrates the brilliance of women across every facet of tech, and signifies the boundless potential of women shaping the future of technology, together!</article>
+    <section className="flex flex-col items-center gap-4 mb-4 font-main">
+        <h3 className="text-3xl font-bold">About Us</h3>
+        <article className="text-xl">The Women Coders Network is a community that fosters connection, ignites knowledge sharing, celebrates the brilliance of women across every facet of tech, and signifies the boundless potential of women shaping the future of technology, together!</article>
     </section>
   );
 };
