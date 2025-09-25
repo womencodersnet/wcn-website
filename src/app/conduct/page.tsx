a86a812d01250516c021a0b783bcafe465a25bf9
Amelia Dutta <amelia280111dutta@gmail.com>
2025-09-25 11:00:08 +0530

    Created one re-usable component for any page that's Under Construction

--- src/app/conduct/page.tsx
+++ src/app/conduct/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Error from "../components/Error";
 
 const CodeOfConduct = () => {
   return (
@@ -7,9 +8,7 @@ const CodeOfConduct = () => {
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
         <h1 className="text-4xl font-bold">Code of Conduct</h1>
-        <p className="mt-4 text-lg">
-          Coming Soon!
-        </p>
+        <Error />
         {/* More content goes here */}
       </main>
     </div>
