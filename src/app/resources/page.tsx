767af8a13c87affa8c7ae4319734a5eefd350711
Amelia Dutta <amelia280111dutta@gmail.com>
2026-03-10 14:42:42 +0530

    feat-71 Added dark mode button to the website to change the theme globally

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,17 +1,21 @@
-'use client';
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Resource from './resource';
-import Footer from '../components/Footer';
+"use client";
+import React from "react";
+import Navbar from "../components/Navbar";
+import Resource from "./resource";
+import Footer from "../components/Footer";
 
 const Resources = () => {
-
   return (
     <div className="landing-body">
       <Navbar />
-      <main className="flex flex-col gap-12 items-center min-h-screen p-4 md:p-8 bg-white text-(--color-dark-violet) font-(family-name:--font-main)">
-        <h3 className="text-3xl font-extrabold mb-6 text-center">Resources</h3>
-        <p className='text-lg'>A collection of resources dedicated to empowering women in tech, curated by past, present, and future women in tech.</p>
+      <main className="flex flex-col gap-12 items-center min-h-screen p-4 md:p-8 text-(--color-dark-violet) font-(family-name:--font-main)">
+        <h3 className="text-3xl font-extrabold mb-6 text-center text-(--color-dark-violet) dark:text-(--color-leafy)">
+          Resources
+        </h3>
+        <p className="text-lg dark:text-(--color-wheat)">
+          A collection of resources dedicated to empowering women in tech,
+          curated by past, present, and future women in tech.
+        </p>
         <Resource />
         <Footer />
       </main>
