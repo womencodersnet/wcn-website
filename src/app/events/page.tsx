0310f0bf83cab177bda071dfee836f015f7885c7
sunsplat <heeycutie@yahoo.com>
2026-08-14 10:49:37 +0400

    add a message if no events found

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -28,7 +28,7 @@ const Events = () => {
       <div className="landing-body">
         <Navbar />
         <main className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
-          <p>Loading...</p>;
+          <p>Loading...</p>
         </main>
       </div>
     );
@@ -43,7 +43,11 @@ const Events = () => {
           <a href="https://calendar.google.com/calendar/u/0/embed?src=7bd85d9630b008f805dfb8ac4e76bc7475c5a84b03ee80674d56beb9a461fd02@group.calendar.google.com&ctz=America/Los_Angeles&pli=1">Subscribe to our events calendar</a> to stay updated about community events.
         </p>
         <div className="max-w-4xl mx-auto">
-          {events && events.map(({ id, location, start, end, htmlLink, summary }: { id: string, location: string, start: { dateTime: string }, end: { dateTime: string }, htmlLink: string, summary: string }) => <Event location={location} startTime={start.dateTime} endTime={end.dateTime} link={htmlLink} title={summary} key={id} />)}
+          { !events.length && <p className="text-center text-md">No upcoming events found</p> }
+          { events 
+            && events.map(({ id, location, start, end, htmlLink, summary }: { id: string, location: string, start: { dateTime: string }, end: { dateTime: string }, htmlLink: string, summary: string }) => {
+            return <Event location={location} startTime={start.dateTime} endTime={end.dateTime} link={htmlLink} title={summary} key={id} />})
+          }
         </div>
       </main>
       <Footer />
